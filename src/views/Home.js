6194aeda1d058398048e65ef8db4ceefaf2499d6
artissimo28 <simona.pirvuceanu@gmail.com>
2022-05-12 20:17:34 +0300

    part 1 - final project

---
+++ src/views/Home.js
@@ -0,0 +1,17 @@
+import { Container, Row } from "reactstrap";
+
+function Home() {
+	return;
+	<Container>
+		<Row>
+			<div
+				style={{
+					height: "100vh",
+					width: "100%",
+					backgroundColor: "grey",
+				}}></div>
+		</Row>
+	</Container>;
+}
+
+export default Home;
