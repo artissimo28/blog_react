2d914099384ac70607d2c976a5a88a1ea2398e6e
artissimo28 <simona.pirvuceanu@gmail.com>
2022-05-26 18:08:12 +0300

    modificari

---
+++ src/components/Home/Post.js
@@ -0,0 +1,49 @@
+import { Col, Button } from "reactstrap";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import {addToWishlist} from "../../helpers";
+
+function Post({ post }) {
+	const [showButton, setShowButton] = useState(false);
+	return (
+		<Col xs='12' md='6'
+			onMouseEnter={() => {
+					setShowButton(true);
+				}}
+				onMouseLeave={() => {
+					setShowButton(false);
+				}}>
+					
+				</Col>>
+			<Link
+				to={"/blog/" + post.id}
+				style={{
+					textDecoration: "none",
+					color: "black",
+				}}
+			
+				<img
+					src={`https://picsum.photos/seed/${post.id}/600`}
+					style={{
+						height: "300px",
+						objectFit: "cover",
+						width: "100%",
+						marginTop: "50px",
+					}}
+				/>
+				<h2>{post.title}</h2>
+				<p>{post.body}</p>
+			</Link>
+			<div style={{ minHeight: "50px" }}>
+				{showButton && <Button outline onClick={(post);
+				}}>
+				Add to wishlist!
+				</Button>
+			)};
+			
+			</div>
+		</Col>
+	);
+}
+
+export default Post;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,67 @@
-import { Container, Row } from "reactstrap";
+import "./Home.css";
+import { Container, Row, Button } from "reactstrap";
+import { useState, useEffect } from "react";
+import Post from "../components/Home/Post";
+import Footer from "../common/Footer";
 
+const counterSize = 8;
 function Home() {
-	return;
-	<Container>
-		<Row>
-			<div
-				style={{
-					height: "100vh",
-					width: "100%",
-					backgroundColor: "grey",
-				}}></div>
-		</Row>
-	</Container>;
+	const [posts, setPosts] = useState(null);
+
+	const [postCounter, setPostCounter] = useState(6);
+
+	const getPosts = async () => {
+		const responseData = await fetch(
+			"https://jsonplaceholder.typecode.com/posts"
+		);
+		const apiPosts = responseData.json();
+		console.log(apiPosts);
+		setPosts(apiPosts);
+	};
+
+	useEffect(() => {
+		getPosts();
+	}, []);
+
+	return (
+		<>
+			<Container>
+				<Row>
+					<div
+						style={{
+							height: "100vh",
+							width: "100%",
+						}}>
+						<img
+							className='cover_image'
+							src='https://images.unsplash.com/photo-1546074177-ffdda98d214f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80'></img>
+					</div>
+				</Row>
+				<Row>
+					{posts ? (
+						<>
+							{posts.slice(0, PostCounter).map((post, index) => {
+								return <Post post={post} key={"post_" + index} />;
+							})}
+						</>
+					) : (
+						<div>Loading...</div>
+					)}
+				</Row>
+				<Row>
+					<Button
+						className='mx-auto mb-4'
+						style={{ width: "200px" }}
+						onClick={() => {
+							setPostCounter(postCounter + counterSize);
+						}}>
+						Show more!
+					</Button>
+				</Row>
+			</Container>
+			<Footer />
+		</>
+	);
 }
 
 export default Home;
